9e38d7f3155bd84ca6528a5b5be7c9c864f05d0f
boris <boris@metitech.eu>
2021-05-13 16:07:12 +0200

    Added a data store for the info blocks.

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Hi there!',
+    headLine: 'Here are some of my projects.',
+    description: 'This website is one of them too!',
+    buttonLabel: 'Take a look',
+    imgStart: false,
+    img: require('../../images/img-1.jpg'),
+    alt: 'img1',
+    dark: true,
+    primary: true,
+    darkText: false
+}
